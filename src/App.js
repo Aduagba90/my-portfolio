38cd7308970eb6442febee32e3c246b740350c70
Lukman <aduagba90@gmail.com>
2026-01-23 09:11:04 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -152,7 +152,7 @@ export default function App() {
       </section>
 
 {/* ===== About Me Section ===== */}
-<section id="about" className="py-24 bg-gray-50 relative">
+<section id="about" className="py-24 bg-gray-50">
   
   <div className="max-w-6xl mx-auto px-4">
     <h2 className="text-3xl md:text-4xl font-bold text-center">
